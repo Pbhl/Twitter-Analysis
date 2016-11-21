ad674ec4c71c3fa4505d09698a40066e8e3ead83
Pbhl <parthb102@googlemail.com>
2016-11-22 03:11:39 +0530

    used api to authenticate

--- twitter_extract.py
+++ twitter_extract.py
@@ -8,4 +8,15 @@ import csv
 import re
 class TwitterDataCollect:
     def __init__(self):
-        
+        import tweepy
+        from tweepy import OAuthHandler
+        # V. IMPORTANT DO I NEED ACCESS TOKEN
+        TWITTER_APP_KEY = 'qQfX6GH7VAXeKmCp2MsayL2wr'
+        TWITTER_APP_KEY_SECRET = 'hquhesVieC9Zb582MgjbODLMXoqJjhncmvpwJaz18QIK2o4F8w'
+        TWITTER_ACCESS_TOKEN = '2842073465-NcwSRO6gUQjhKMD77oIe2stt5gtzmiHtLLGFjgJ'
+        TWITTER_ACCESS_TOKEN_SECRET = '0aLJAH0zn9ImxqpBtfC65ffcC9GVxkvyGcnZNmQI36p7j'
+
+        self.api = Twython(app_key=TWITTER_APP_KEY,
+                           app_secret=TWITTER_APP_KEY_SECRET,
+                           oauth_token=TWITTER_ACCESS_TOKEN,
+                           oauth_token_secret=TWITTER_ACCESS_TOKEN_SECRET)

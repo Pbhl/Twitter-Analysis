e499a6bef55057e51675d159b91ee7c56bb952f9
Pbhl <parthb102@googlemail.com>
2016-11-23 17:54:01 +0530

    Added functions to format tweets

--- twitter_extract.py
+++ twitter_extract.py
@@ -20,3 +20,30 @@ class TwitterDataCollect:
                            app_secret=TWITTER_APP_KEY_SECRET,
                            oauth_token=TWITTER_ACCESS_TOKEN,
                            oauth_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
+     def read_words(self,words_file):
+            return [word for line in open(words_file, 'r', encoding="utf8") for word in line.split()]  
+     def strip_non_ascii(self, txt):
+        stripped = (c for c in txt if ((c in self.emot) or (0 < ord(c) < 127)))
+        return ''.join(stripped)
+     def process_tweet(self, tweet):
+        #Remove non-ascii characters
+        tweet = self.strip_non_ascii(tweet)
+        # Convert to lower case
+        tweet = tweet.lower()
+        #Remove b' leading characters
+        #tweet = tweet[2:]
+        # Convert www.* or https?://* to blank
+        tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', '', tweet)
+        # Convert @username to blank
+        tweet = re.sub('@[^\s]+', '', tweet)
+        # Replace #word with word
+        tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+        #Remove repeating characters
+        tweet = re.sub(r'(.)\1\1+', r'\1', tweet)
+        #Remove words starting with numbers
+        tweet = re.sub(r'(\s)\d\w+', '', tweet)
+        # Remove additional white spaces
+        tweet = re.sub('[\s]+', ' ', tweet)
+        # trim
+        tweet = tweet.strip('\'"')
+        return tweet
